528bdef1560538a13ce9e9abacf04c9c389c44b9
GrinsteadDev <devin.grinstead@grinsteaddev.com>
2024-12-17 09:23:37 -0500

    added light mode styles and checked input type to radio

--- dyslexic pages/js/index.js
+++ dyslexic pages/js/index.js
@@ -34,7 +34,6 @@ function setCurrentFont(font) {
 
 window.DyslexicPages.fonts.then((fontData) => {
     const defaultText = "AaBbCcDdEeFfGg";
-    let inCheckEvent = false;
     const fontCardTemp = document.querySelector('template#font-card');
     const dBody = document.querySelector('.body');
     const inputBox = document.querySelector('input[type="text"]#preview');
@@ -54,24 +53,9 @@ window.DyslexicPages.fonts.then((fontData) => {
             cpy.querySelector('.author').href = font['author']['link'];
             cpy.querySelector('.author').innerText = font['author']['name'];
             cpy.querySelector('.display').innerText = defaultText;
-            cpy.querySelector('.toggle input[type="checkbox"]').checked = (font['name'] == dFont['name']);
+            cpy.querySelector('.toggle input[type="radio"]').checked = (font['name'] == dFont['name']);
             
-            cpy.querySelector('.toggle input[type="checkbox"]').addEventListener('change', (e) => {
-                if (!inCheckEvent) {
-                    inCheckEvent = true;
-
-                    let elms = dBody.querySelectorAll('.font-card .toggle input[type="checkbox"]:checked')
-                    elms.forEach((elm) => {
-                        if (e.currentTarget !== elm) {
-                            elm.checked = false;
-                        }
-                    });
-                    if (!e.currentTarget.checked && elms.length > 0) {
-                        elms[0].checked = true;
-                    }
-
-                    inCheckEvent = false;
-                }
+            cpy.querySelector('.toggle input[type="radio"]').addEventListener('change', (e) => {
                 if (e.currentTarget.checked) {
                     setCurrentFont(font);
                 }
@@ -81,7 +65,7 @@ window.DyslexicPages.fonts.then((fontData) => {
         });
 
         const displays = dBody.querySelectorAll('.font-card .display');
-        const checkBoxes = dBody.querySelectorAll('.font-card .toggle input[type="checkbox"]');
+        const checkBoxes = dBody.querySelectorAll('.font-card .toggle input[type="radio"]');
 
         inputBox.addEventListener('keyup', (e) => {
             displays.forEach((elm) => {
