46d78b0ee5ba4600ade2b460d9a9d2b807b0d2ac
GrinsteadDev <devin.grinstead@grinsteaddev.com>
2025-03-11 13:37:35 -0400

    Update

--- dyslexic pages/js/fonts-page.js
+++ dyslexic pages/js/fonts-page.js
@@ -6,7 +6,6 @@
     function AddFont(font, fontTile, container, defaultText)
     {
         let cpy = fontTile.content.cloneNode(true);
-        let fTile = cpy.querySelector('.font-tile');
         let fName = cpy.querySelector('.name');
         let fAuthor = cpy.querySelector('.author');
         let fDisplay = cpy.querySelector('.display');
@@ -72,3 +71,127 @@
         }
     });
 })();
+
+let fontSizeCombo = document.querySelector('#font-size-select');
+let fontSizeOptions = document.querySelectorAll('#font-size-select option');
+let fontSizeSlider = document.querySelector('#font-size-slider');
+let customSizeCombo = document.createElement('div');
+let customSizeComboDisplay = document.createElement('span');
+let customSizeComboDisplayArrow = document.createElement('span');
+let customSizeComboOptions = document.createElement('div');
+
+fontSizeCombo.parentElement.appendChild(customSizeCombo);
+
+customSizeCombo.appendChild(customSizeComboDisplay);
+customSizeCombo.appendChild(customSizeComboDisplayArrow);
+customSizeCombo.appendChild(customSizeComboOptions);
+customSizeCombo.setAttribute('tabindex', '0');
+customSizeCombo.style.setProperty('width', '100%');
+customSizeCombo.style.setProperty('padding-bottom', '5px');
+customSizeCombo.style.setProperty('overflow-y', 'visible');
+customSizeCombo.style.setProperty('position', 'relative');
+customSizeCombo.style.setProperty('cursor', 'pointer');
+customSizeCombo.style.setProperty('background-color', 'inherit');
+
+customSizeComboDisplay.innerText = "\u00A0";
+customSizeComboDisplay.classList.add('focus');
+customSizeComboDisplay.style.setProperty('border-radius', '15px');
+customSizeComboDisplay.style.setProperty('display', 'inline-block');
+
+customSizeComboDisplayArrow.style.setProperty('display', 'inline-block');
+customSizeComboDisplayArrow.style.setProperty('margin-left', '5px');
+customSizeComboDisplayArrow.style.setProperty('border-left', '7px solid transparent');
+customSizeComboDisplayArrow.style.setProperty('border-right', '7px solid transparent');
+customSizeComboDisplayArrow.style.setProperty('border-top', '13px solid var(--font-color)');
+
+customSizeComboOptions.style.setProperty('display', 'none');
+customSizeComboOptions.style.setProperty('overflow-y', 'scroll');
+customSizeComboOptions.style.setProperty('height', '25vh');
+customSizeComboOptions.style.setProperty('position', 'absolute');
+customSizeComboOptions.style.setProperty('top', '100%');
+customSizeComboOptions.style.setProperty('left', '0');
+customSizeComboOptions.style.setProperty('background-color', 'inherit');
+customSizeComboOptions.style.setProperty('border-radius', 'inherit');
+customSizeComboOptions.style.setProperty('border-top-left-radius', '0');
+customSizeComboOptions.style.setProperty('border-top-right-radius', '0');
+customSizeComboOptions.classList.add('custom-scroll-bar');
+
+fontSizeCombo.addEventListener('change', ()=>{
+    fontSizeSlider.value = parseInt(fontSizeCombo.value);
+
+});
+fontSizeSlider.addEventListener('change', ()=>{
+    fontSizeCombo.options[0].innerText = fontSizeSlider.value + 'px';
+    fontSizeCombo.selectedIndex = 0;
+    customSizeComboDisplay.innerText = fontSizeSlider.value + 'px';
+});
+fontSizeOptions.forEach((obj, idx)=>{
+    let opt = document.createElement('span');
+
+    opt.innerText = obj.innerText;
+    opt.classList.add('focus');
+    opt.setAttribute('tabindex', '0');
+    opt.style.setProperty('border-radius', '15px');
+    opt.style.setProperty('margin', '3px 1.5px 3px 1.5px');
+if (idx == 0)
+    {
+        opt.style.setProperty('display', 'none');
+    } else {
+        opt.style.setProperty('display', 'block');
+        opt.addEventListener('click', ()=>{
+            fontSizeCombo.selectedIndex = idx;
+            customSizeComboDisplay.innerText = opt.innerText;
+            fontSizeCombo.dispatchEvent(new Event('change'));
+        });
+    }
+    if (idx == fontSizeCombo.selectedIndex)
+    {
+        customSizeComboDisplay.innerText = opt.innerText;
+    }
+
+    customSizeComboOptions.appendChild(opt);
+});
+customSizeCombo.addEventListener('click', ()=>{
+    if (customSizeComboOptions.style.getPropertyValue('display') == 'none')
+    {
+        customSizeComboOptions.style.setProperty('display', 'block');
+        customSizeComboDisplayArrow.style.setProperty('border-bottom', '13px solid var(--font-color)');
+        customSizeComboDisplayArrow.style.removeProperty('border-top');
+    } else {
+        customSizeComboOptions.style.setProperty('display', 'none');
+        customSizeComboDisplayArrow.style.setProperty('border-top', '13px solid var(--font-color)');
+        customSizeComboDisplayArrow.style.removeProperty('border-bottom');
+    }
+});
+customSizeCombo.addEventListener('focusout', (e)=>{
+    if (!customSizeCombo.contains(e.relatedTarget))
+    {
+        customSizeComboOptions.style.setProperty('display', 'none');
+        customSizeComboDisplayArrow.style.setProperty('border-top', '13px solid var(--font-color)');
+        customSizeComboDisplayArrow.style.removeProperty('border-bottom');
+    }
+});
+customSizeCombo.addEventListener('keyup', (e)=>{
+    if (e.key == 'Enter')
+    {
+        customSizeCombo.dispatchEvent(new Event('click'));
+    }
+});
+
+let fontPreview = document.querySelector('#fonts-page #font-preview');
+
+fontPreview.addEventListener('keyup', (e)=>{
+    let elements = document.querySelectorAll('#fonts-page .font-tile > .display');
+    
+    elements.forEach((obj, idx)=>{
+        if (fontPreview.value.trim() != '')
+        {
+            if (fontPreview.value.trim().length > 500)
+                fontPreview.value = fontPreview.value.trim().substring(0, 499);
+    
+            obj.innerText = fontPreview.value.trim();
+        } else {
+            obj.innerText = 'AbBbCcDdEeFfGg';
+        }
+    });
+});
